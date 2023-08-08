825b2556b615bd90a96bd4325d67f018c93f6e83
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-08-08 11:49:07 +0000

    chore: create business logic for user login

--- app/Http/Controllers/v1/Auth/LoginController.php
+++ app/Http/Controllers/v1/Auth/LoginController.php
@@ -3,16 +3,44 @@
 namespace App\Http\Controllers\v1\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\JsonResponse;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\JsonResponse;
+
 
 class LoginController extends Controller
 {
     // Login Route
-    public function login() : JsonResponse 
+    public function login(Request $request) : JsonResponse 
     {
-        return response()->json([
-            'message' => 'User Logged in'
-        ], 200);
+        $request->validate([
+            'login_field' => 'required|string',
+            'password' => 'required|string',
+        ]);
+    
+        $loginField = $request->input('login_field');
+
+        if (filter_var($loginField, FILTER_VALIDATE_EMAIL)) 
+        {
+            $credentials = [
+                'email' => $loginField,
+                'password' => $request->input('password'),
+            ];
+        } else {
+            $credentials = [
+                'username' => $loginField,
+                'password' => $request->input('password'),
+            ];
+        }
+
+        if (Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'User Logged in'
+            ], 200);
+        } else {
+            throw ValidationException::withMessages(['login_field' => 'Invalid credentials']);
+        }
     }
 }

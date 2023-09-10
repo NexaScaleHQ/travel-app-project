3f7bdc94c8eba768692370434cebd806cf29e8f6
Adeola Ephraim <62262920+codeDeeAi@users.noreply.github.com>
2023-09-10 01:23:58 +0100

    [ADD] added savedlocation migration and model

---
+++ database/migrations/2023_09_10_002010_create_saved_locations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('saved_locations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('location_name');
+            $table->dateTime('date_saved', 0);
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('saved_locations');
+    }
+};

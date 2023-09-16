f078abdf05086cbe50d109f10b56a8431b53d48a
David Chibueze Ndubuisi <davydocsurg@gmail.com>
2023-09-16 10:46:39 +0100

    Update 2023_09_16_080713_create_hotels_table.php

--- database/migrations/2023_09_16_080713_create_hotels_table.php
+++ database/migrations/2023_09_16_080713_create_hotels_table.php
@@ -15,16 +15,16 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
             $table->string('hotel_name');
-            $table->string('description');
+            $table->longText('description');
             $table->integer('location');
             $table->integer('rating');
             $table->integer('price_range');
-            $table->string('amenities');
+            $table->longText('amenities');
             $table->dateTime('check_in_time');
             $table->dateTime('check_out_time');
             $table->boolean('is_wifi_available');
             $table->string('contact_information');
-            $table->string('reviews');
+            $table->longText('reviews');
             $table->timestamps();
         });
     }

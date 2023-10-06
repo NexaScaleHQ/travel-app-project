21687221191eb1bfffba86a1d4da23b7a61276ee
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-10-06 06:38:46 +0100

    feat: Implement user login

--- app/Http/Controllers/v1/Auth/LoginController.php
+++ app/Http/Controllers/v1/Auth/LoginController.php
@@ -3,16 +3,35 @@
 namespace App\Http\Controllers\v1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     // Login Route
     public function login() : JsonResponse 
-    {
-        return response()->json([
-            'message' => 'User Logged in'
-        ], 200);
+    {   
+        try {
+            $user = User::where('email', request('email'))->first();
+
+            if (!$user || !Hash::check(request('password'), $user->password)) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login Successful',
+                'token' => $token,
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Invalid Credentials', 
+                'error' => $th->getMessage()
+            ], 401);
+        }
     }
 }

f04338d1040277707cc1cfa1644ae06a53ed26fd
Ani Chukwuebuka <anichukwuebuka65@gmail.com>
2023-10-20 20:40:25 +0100

    made some changes

--- app/Http/Controllers/v1/Auth/RegisterController.php
+++ app/Http/Controllers/v1/Auth/RegisterController.php
@@ -13,22 +13,38 @@ class RegisterController extends Controller
 {
     public function registerUser(Request $request)
     {
-        $validator = Validator::make($request->all(),[
-                'first_name' => 'required',
-                'last_name' => 'required',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required|confirmed',
-            ]);
+        $validator = $this->validator( $request );
 
         if ($validator->fails()) {
-            return response($validator->errors(), 401);
+            return response($validator->errors(), 400);
         }
 
-        $user = User::create([...$validator->validated(), 'password' => Hash::make($validator->validated()['password'])]);
-         
-        if($user) {
-            $token = $user->createToken('token')->plainTextToken;
-            return ['token' => $token];
-        }
+        try {
+            $user = User::create([...$validator->validated(), 'password' => Hash::make($validator->validated()['password'])]);
+            
+            if($user) {
+                $token = $user->createToken('token')->plainTextToken;
+
+                return [
+                    'message' => 'user registered successfully',
+                    'token' => $token,
+                    'user' => $user,
+                ];
+            }
+        } catch (\Throwable $th) {
+            return response('Something went wrong', 500);
+        } 
+    }
+
+    public function validator(Request $request){
+
+        $validator = Validator::make($request->all(),[
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
+
+        return $validator;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,3 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', [RegisterController::class, 'registerUser']);
-
-
-
-
